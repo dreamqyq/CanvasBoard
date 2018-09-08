d23f3b5115aa983b4dde7954e6ab188e488959b4
enoch <qyqdream@gmail.com>
2018-09-08 18:17:27 +0800

    update: 优化代码(useMouthOrTouch)

--- js/main.js
+++ js/main.js
@@ -7,180 +7,163 @@ class Draw {
     this.domView = this.byId(this.view)
     this.context = this.domView.getContext('2d')
     this.isClear = false
+    this.isUsing = false
+    this.drawWidth = 1
     this.autoResizeCanvas()
     this.userAction(this.domView)
     this.changeSize()
   }
 
   byId(tag) {
-    return document.getElementById(tag);
+    return document.getElementById(tag)
+  }
+
+  autoResizeCanvas() {
+    this._canvasInit()
+    window.onresize = () => {
+      this._canvasInit()
+    }
+  }
+
+  userAction(tag) {
+    this.context.lineWidth = 2
+    // 用户选择笔刷or橡皮擦
+    this._userSelect()
+    // 笔刷重置（笔刷大小颜色）
+    this._brushReset()
+    // 图像下载
+    this.downloadPic()
+    // 特性检测
+    if (document.body.ontouchstart !== undefined) {
+      // 支持触摸事件
+      this._useMouthOrTouch(tag,['touchstart','touchmove','touchend',true])
+    } else {
+      //不支持触摸事件
+      this._useMouthOrTouch(tag,['mousedown','mousemove','mouseup',false])
+    }
   }
 
   changeSize() {
     let sizeBtn = this.byId('size')
     let sizeRanks = sizeBtn.children
-    let num = 1
     sizeBtn.addEventListener('click', () => {
       for (let i = 0; i < sizeRanks.length; i++) {
         sizeRanks[i].classList.remove('active')
       }
-      sizeRanks[num].classList.add('active')
-      this.context.lineWidth = (num + 1) * 2;
-      num += 1
-      if (num >= sizeRanks.length) {
-        num = 0
+      sizeRanks[this.drawWidth].classList.add('active')
+      this.context.lineWidth = (this.drawWidth + 1) * 2
+      this.drawWidth += 1
+      if (this.drawWidth >= sizeRanks.length) {
+       this.drawWidth = 0
       }
     })
   }
 
-  canvasInit(tag) {
-    tag.width = document.documentElement.clientWidth;
-    tag.height = document.documentElement.clientHeight;
+  _canvasInit() {
+    this.domView.width = document.documentElement.clientWidth
+    this.domView.height = document.documentElement.clientHeight
+    this.context.lineWidth = this.drawWidth
   }
 
-  autoResizeCanvas() {
-    this.canvasInit(canvas);
-
-    window.onresize = function () {
-      this.canvasInit(canvas);
-    }
-
-  }
 
   drawLine(x1, y1, x2, y2) {
-    this.context.beginPath();
-    this.context.moveTo(x1, y1);
-    this.context.lineTo(x2, y2);
-    this.context.stroke();
-    this.context.closePath();
+    this.context.beginPath()
+    this.context.moveTo(x1, y1)
+    this.context.lineTo(x2, y2)
+    this.context.stroke()
+    this.context.closePath()
   }
 
   drawCircle(x, y) {
-    this.context.beginPath();
-    this.context.arc(x, y, 2, 0, Math.PI * 2);
-    this.context.fill();
+    this.context.beginPath()
+    this.context.arc(x, y, 2, 0, Math.PI * 2)
+    this.context.fill()
   }
 
   //绘制矩形
   drawRect(x1, y1, x2, y2, color) {
-    color = color || '#000';
-    this.context.fillStyle = color;
-    this.context.fillRect(x1, y1, x2, y2);
+    color = color || '#000'
+    this.context.fillStyle = color
+    this.context.fillRect(x1, y1, x2, y2)
   }
-  userAction(tag) {
-    var isUsing = false,
-      lastPoint = {
-        x: undefined,
-        y: undefined
-      };
-    this.context.lineWidth = 2;
-    // 用户选择笔刷or橡皮擦
-    this.userSelect();
-    // 笔刷重置（笔刷大小颜色）
-    this.brushReset();
-    // 图像下载
-    this.downloadPic();
-    // 特性检测
-    if (document.body.ontouchstart !== undefined) {
-      // 支持触摸事件
-      tag.addEventListener( 'touchstart' ,  point => {
-        var pointX = point.touches[0].clientX,
-          pointY = point.touches[0].clientY;
-        isUsing = true;
-        if (this.isClear) {
-          this.eraserFn(pointX, pointY);
-        } else {
-          lastPoint.x = pointX;
-          lastPoint.y = pointY;
-          this.drawCircle(pointX, pointY);
-        }
-      })
-      tag.addEventListener('touchmove', point => {
-        var pointX = point.touches[0].clientX,
-          pointY = point.touches[0].clientY,
-          newPoint = {
-            x: pointX,
-            y: pointY
-          };
 
-        if (!isUsing) {
-          return
-        }
-        if (isUsing) {
-          if (this.isClear) {
-            this.eraserFn(pointX, pointY);
-          } else {
-            this.drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-          }
-        }
-        lastPoint.x = pointX;
-        lastPoint.y = pointY;
-      })
-      tag.addEventListener('touchend', point => {
-        isUsing = false;
-      })
-
-    } else {
-      //不支持触摸事件
-      tag.addEventListener('mousedown', point => {
-        var pointX = point.clientX,
-          pointY = point.clientY;
-        isUsing = true;
-        if (this.isClear) {
-          this.eraserFn(pointX, pointY);
-        } else {
-          lastPoint.x = pointX;
-          lastPoint.y = pointY;
-          this.drawCircle(pointX, pointY);
-        }
-      })
-
-      tag.addEventListener('mousemove', point => {
-        var pointX = point.clientX,
-          pointY = point.clientY,
-          newPoint = {
-            x: pointX,
-            y: pointY
-          };
-
-        if (!isUsing) { return }
-        if (isUsing) {
-          if (this.isClear) {
-            this.eraserFn(pointX, pointY);
-          } else {
-            this.drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-          }
-        }
-        lastPoint.x = pointX;
-        lastPoint.y = pointY;
-      })
-      tag.addEventListener('mouseup', point => {
-        isUsing = false;
-      })
+  _returnPoint(boolean,point) {
+    if(boolean){
+      // touch
+      return{
+        pointX: point.touches[0].clientX,
+        pointY: point.touches[0].clientY
+      } 
+    }else{
+      // mouse
+      return{
+        pointX: point.clientX,
+        pointY: point.clientY
+      }
     }
   }
+  _useMouthOrTouch(tag,methods) {
+    let lastPoint = {
+      x: undefined,
+      y: undefined
+    }
+    tag.addEventListener(methods[0], point => {
+      let pointX = this._returnPoint(methods[3],point).pointX
+      let pointY = this._returnPoint(methods[3],point).pointY
+      this.isUsing = true
+      if (this.isClear) {
+        this.eraserFn(pointX, pointY)
+      } else {
+        lastPoint.x = pointX
+        lastPoint.y = pointY
+        this.drawCircle(pointX, pointY)
+      }
+    })
+    tag.addEventListener(methods[1], point => {
+      let pointX = this._returnPoint(methods[3],point).pointX
+      let pointY = this._returnPoint(methods[3],point).pointY
+      let newPoint = {
+        x: pointX,
+        y: pointY
+      }
 
-  userSelect() {
-    var brushBtn = this.byId('brush'),
-      eraserBtn = this.byId('eraser');
+      if (this.isUsing) {
+        if (this.isClear) {
+          this.eraserFn(pointX, pointY)
+        } else {
+          this.drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        }
+      } else {
+        return
+      }
+      lastPoint.x = pointX
+      lastPoint.y = pointY
+    })
+    tag.addEventListener(methods[2], point => {
+      this.isUsing = false
+    })
+  }
+  _userSelect() {
+    let brushBtn = this.byId('brush')
+    let eraserBtn = this.byId('eraser')
     brushBtn.addEventListener('click', () => {
-      this.isClear = false;
-      eraserBtn.classList.remove('svgActive');
-      brushBtn.classList.add('svgActive');
+      this.isClear = false
+      eraserBtn.classList.remove('svgActive')
+      brushBtn.classList.add('svgActive')
     })
     eraserBtn.addEventListener('click', () => {
-      this.isClear = true;
-      eraserBtn.classList.add('svgActive');
-      brushBtn.classList.remove('svgActive');
+      this.isClear = true
+      eraserBtn.classList.add('svgActive')
+      brushBtn.classList.remove('svgActive')
     })
   }
 
   // 橡皮擦函数
   eraserFn(x, y) {
-    this.context.clearRect(x - 5, y - 5, 10, 10);
+    this.context.clearRect(x - 5, y - 5, 10, 10)
   }
 
-  colorListener(element, eventType, selector, callback) {
+  _colorListener(element, eventType, selector, callback) {
     element.addEventListener(eventType, event => {
       let currentColor = event.target
       if (currentColor.matches(selector)) {
@@ -192,39 +175,38 @@ class Draw {
   }
 
   //按钮功能函数
-  brushReset() {
-    var clearBtn = this.byId('delete'),
-      brushColors = this.byId('brushColor');
+  _brushReset() {
+    let clearBtn = this.byId('delete')
+    let brushColors = this.byId('brushColor')
     clearBtn.addEventListener('click', () => {
-      this.drawRect(0, 0, canvas.width, canvas.height, '#fff');
-    }) 
-
+      this.drawRect(0, 0, canvas.width, canvas.height, '#fff')
+    })
     //颜色更改
-    this.colorListener(brushColors, 'click', 'li', (event, current, color, siblings, parent) => {
+    this._colorListener(brushColors, 'click', 'li', (event, current, color, siblings, parent) => {
       this.context.fillStyle = color
       this.context.strokeStyle = color
-      for(let i = 0; i < siblings.length; i++){
+      for (let i = 0; i < siblings.length; i++) {
         siblings[i].classList.remove('btnActive')
       }
-      current.classList.add('btnActive');
+      current.classList.add('btnActive')
     })
   }
 
-    // 下载图像函数
-    downloadPic() {
-      var downloadBtn = this.byId('download'),
-        body = this.byId('body');
-      downloadBtn.onclick = function () {
-        var url = canvas.toDataURL('img/png');
-        var aTag = document.createElement('a');
-        body.appendChild(aTag);
-        aTag.href = url;
-        aTag.download = 'mypic';
-        aTag.target = '_blank';
-        aTag.click();
-      }
+  // 下载图像函数
+  downloadPic() {
+    let downloadBtn = this.byId('download')
+    let body = this.byId('body')
+    downloadBtn.onclick = function () {
+      let url = canvas.toDataURL('img/png')
+      let aTag = document.createElement('a')
+      body.appendChild(aTag)
+      aTag.href = url
+      aTag.download = 'mypic'
+      aTag.target = '_blank'
+      aTag.click()
     }
   }
+}
 
 let draw = new Draw('canvas')
 draw.init.call(draw)
